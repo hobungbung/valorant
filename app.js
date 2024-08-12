0653db463a330add7ce5d15cc29b79196551ffbf
hobungbung <tftkjw06@gmail.com>
2024-08-12 19:00:24 +0900

    처음으로 만든 웹사이트 ( 발로란트 요원 설명)

---
+++ app.js
@@ -0,0 +1,19 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const a = 4;
+
+const dayOfWeek = [mon, tue, wed, thu, fri, sat];
+
+console.log(dayOfWeek[a]);
+
+dayOfWeek.push("sun");
+
+console.log(dayOfWeek[4]);
+
+let abc = "endless";
